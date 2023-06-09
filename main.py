988ae05aa050d3aff07cac5e72900951aa84f7e3
Thom <thomasbandy23@gmail.com>
2023-06-10 00:09:33 +1000

    Refactored

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-# Author: Thomas Bandy
-# Project: PDF Merger
-#
+"""
+Author: Thomas Bandy
+Project: PDF Merger
+
+This script merges multiple PDF files into a single PDF file using the PyPDF2 library.
+"""
+
 
 import os
 from os.path import dirname, abspath, join
@@ -18,15 +22,25 @@ INPUT_DIR: Final[str] = join(ROOT, "input")
 The directory where the files to be joined are stored.
 """
 
-if __name__ == "__main__":
-    x = [a for a in os.listdir(INPUT_DIR) if a.endswith(".pdf")]
 
+def merge_pdf_files():
+    """
+    Merge the PDF files in the INPUT_DIR directory and save the result as 'result.pdf'.
+    """
+    pdf_files = [
+        filename for filename in os.listdir(INPUT_DIR) if filename.endswith(".pdf")
+    ]
     merger = PdfMerger()
 
-    for pdf in x:
-        merger.append(open(os.path.join(INPUT_DIR, pdf), "rb"))
+    for pdf_file in pdf_files:
+        pdf_path = os.path.join(INPUT_DIR, pdf_file)
+        merger.append(open(pdf_path, "rb"))
 
-    with open("result.pdf", "wb") as out:
-        merger.write(out)
+    with open("result.pdf", "wb") as output_file:
+        merger.write(output_file)
 
     print("Complete")
+
+
+if __name__ == "__main__":
+    merge_pdf_files()
